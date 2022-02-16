88672143c5e801814fe0ac3d7d2b5d5c54e8c018
jvab <jvab@aluno.ifnmg.edu.br>
2022-02-16 11:49:35 -0300

    first commit

---
+++ backend/api/auth.js
@@ -0,0 +1,61 @@
+const { authSecret } = require('../.env')
+const bcrypt = require('bcrypt-nodejs')
+
+module.exports = app => {
+    const signin = async (req, res) => {
+        const encode = require('jwt-encode')
+
+        try {
+            if (!req.body.email || !req.body.password) throw 'É necessário preencher os campos.'
+
+            const userData = await app.db('users')
+                .where({ email: req.body.email })
+                .first()
+
+            if (!userData) throw 'Usuário não encontrado.'
+
+            const validator = bcrypt.compareSync(req.body.password, userData.password)
+            
+            if (!validator) throw 'Email/Senha inválidos.'
+            
+            const now = Date.now() / 1000
+            
+            const payload = {
+                id: userData.id,
+                name: userData.name,
+                email: userData.email,
+                admin: userData.admin || false,
+                iat:now,
+                exp: now + (60 * 60 * 24)
+            }
+            const token = encode(payload, authSecret)
+            res.json({
+                ...payload,
+                token: token
+
+            })
+        } catch (msg) {
+            return res.status(400).send(msg)
+        }
+
+    }
+
+    const validateToken = (req, res) => {
+        const userData = req.body || null
+        const decode = require('jwt-decode')
+
+        try {
+            if (userData) {
+                const token = decode(userData.token)
+                console.log(token)
+                if (new Date(token.exp * 1000) > new Date()) return res.status(201).send(true)
+            }
+        } catch (e) {
+
+        }
+        res.send(false)
+
+    }
+
+    return { signin, validateToken }
+}
